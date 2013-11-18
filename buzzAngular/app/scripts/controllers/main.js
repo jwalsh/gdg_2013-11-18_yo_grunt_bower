9069612667fdeff37f2a8c5ecf01c36d1ce0607e
Jason Walsh <jwalsh@bluekai.com>
2013-11-18 15:51:08 -0800

    driveChrome

---
+++ buzzAngular/app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('buzzAngularApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
